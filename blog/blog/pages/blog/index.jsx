33913d1692ad41ed0252f90cda31902c50d0b175
kato-rakuweb <kato@rakuweb.jp>
2023-02-01 14:29:49 +0900

    2.10

---
+++ blog/blog/pages/blog/index.jsx
@@ -0,0 +1,5 @@
+import Hero from "@/components/hero";
+
+export default function Blog() {
+  return <Hero title="blog" subtitle="Recent Posts" />;
+}
--- blog/blog/pages/index.jsx
+++ blog/blog/pages/index.jsx
@@ -1,19 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
+import Hero from "@/components/hero";
+import Layout from "@/components/layout";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <header>HEADER</header>
-      <div>
-        <h1>CUBE</h1>
-        <p>アウトプットしていくサイト</p>
-      </div>
-      <footer>FOOTER</footer>
+      <Hero title="cube" subtitle="アウトプットしていくサイト" imageOn />
     </>
   );
 }
